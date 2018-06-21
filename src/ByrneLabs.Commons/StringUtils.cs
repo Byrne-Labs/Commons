1ad39220ab64b2a2adf7602deaf7b57697af8f0a
jonathan.byrne <jonathan.byrne@yahoo.com>
2018-06-21 11:31:22 -0400

    Added more string utils

--- src/ByrneLabs.Commons/StringUtils.cs
+++ src/ByrneLabs.Commons/StringUtils.cs
@@ -1,9 +1,87 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnsureThat;
+using JetBrains.Annotations;
 
 namespace ByrneLabs.Commons
 {
+    [PublicAPI]
     public static class StringUtils
     {
+        public static int ContainsCount(this string value, string substring, StringComparison stringComparison = StringComparison.Ordinal)
+        {
+            var currentIndex = 0;
+            var count = 0;
+            while (currentIndex <= value.Length - substring.Length && currentIndex >= 0)
+            {
+                currentIndex = value.IndexOf(substring, currentIndex, stringComparison);
+                if (currentIndex >= 0)
+                {
+                    count++;
+                    currentIndex = currentIndex + substring.Length;
+                }
+            }
+
+            return count;
+        }
+
+        public static int NthIndexOf(this string value, string substring, int count, StringComparison stringComparison = StringComparison.Ordinal)
+        {
+            Ensure.That(value).IsNotNull();
+            Ensure.That(substring).IsNotNullOrEmpty();
+            Ensure.That(count).IsGt(0);
+
+            var foundCount = 0;
+            var index = 0;
+            var nextIndex = 0;
+
+            do
+            {
+                index = value.IndexOf(substring, nextIndex, stringComparison);
+                if (index >= 0)
+                {
+                    foundCount++;
+                    nextIndex = index + substring.Length;
+                }
+            } while (index >= 0 && foundCount < count);
+
+            return index;
+        }
+
+        public static (int, int) GetLineAndColumnNumber(this string value, int index)
+        {
+            Ensure.That(value).IsNotNull();
+            Ensure.That(index).IsGte(0);
+            Ensure.That(index).IsLt(value.Length);
+
+            var line = value.Substring(0, index).ContainsCount("\n") + 1;
+            var column = line == 1 ? index + 1 : index - value.NthIndexOf("\n", line - 1);
+
+            return (line, column);
+        }
+
+        public static string Join(this IEnumerable<string> value, string seperator) => string.Join(seperator, value);
+
         public static string Repeat(this string value, int count) => new StringBuilder(value.Length * count).Insert(0, value, count).ToString();
+
+        public static string SubstringAfterLast(this string value, string substring, StringComparison stringComparison = StringComparison.Ordinal)
+        {
+            Ensure.That(value).IsNotNullOrEmpty();
+            Ensure.That(substring).IsNotNullOrEmpty();
+
+            var lastIndex = value.LastIndexOf(substring, stringComparison);
+            string result;
+            if (lastIndex < 0)
+            {
+                result = string.Empty;
+            }
+            else
+            {
+                result = value.Substring(lastIndex + substring.Length);
+            }
+
+            return result;
+        }
     }
 }
---
+++ test/ByrneLabs.Commons.Tests/StringUtilsTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ByrneLabs.Commons.Tests
+{
+    [Trait("Category", "Unit Test")]
+  public class StringUtilsTest
+    {
+        [Fact]
+        public void TestContainsCount()
+        {
+            Assert.Equal(0, "asdf".ContainsCount("fdsa"));
+            Assert.Equal(1, "asdf".ContainsCount("asdf"));
+            Assert.Equal(1, "asdf".ContainsCount("as"));
+            Assert.Equal(1, "asdf".ContainsCount("sd"));
+            Assert.Equal(1, "asdf".ContainsCount("df"));
+            Assert.Equal(2, "asdfasdf".ContainsCount("asdf"));
+            Assert.Equal(2, "asdfasdf".ContainsCount("as"));
+            Assert.Equal(2, "asdfasdf".ContainsCount("sd"));
+            Assert.Equal(2, "asdfasdf".ContainsCount("df"));
+        }
+
+        [Fact]
+        public void TestNthIndexOf()
+        {
+            Assert.Equal(-1, "asdfasdfasdf".NthIndexOf("fdsa", 1));
+            Assert.Equal(0, "asdfasdfasdf".NthIndexOf("asdf", 1));
+            Assert.Equal(4, "asdfasdfasdf".NthIndexOf("asdf", 2));
+            Assert.Equal(8, "asdfasdfasdf".NthIndexOf("asdf", 3));
+            Assert.Equal(-1, "asdfasdfasdf".NthIndexOf("asdf", 4));
+        }
+
+
+        [Fact]
+        public void TestSubstringAfterLast()
+        {
+            Assert.Equal(string.Empty, "asdfasdf".SubstringAfterLast("fdsa"));
+            Assert.Equal("df", "asdf".SubstringAfterLast("as"));
+            Assert.Equal(string.Empty, "asdf".SubstringAfterLast("df"));
+            Assert.Equal("df", "asdfasdf".SubstringAfterLast("as"));
+            Assert.Equal(string.Empty, "asdfasdf".SubstringAfterLast("df"));
+        }
+
+        [Fact]
+        public void TestGetLineAndColumnNumber()
+        {
+            Assert.Equal((1, 1), "asdfasdfasdf".GetLineAndColumnNumber(0));
+            Assert.Equal((1, 1), "asdf\nasdf\nasdf".GetLineAndColumnNumber(0));
+
+            Assert.Equal((1, 5), "asdfasdfasdf".GetLineAndColumnNumber(4));
+            Assert.Equal((1, 5), "asdf\nasdf\nasdf".GetLineAndColumnNumber(4));
+
+            Assert.Equal((2, 1), "asdf\nasdf\nasdf".GetLineAndColumnNumber(5));
+
+            Assert.Equal((2, 2), "asdf\nasdf\nasdf".GetLineAndColumnNumber(6));
+
+            Assert.Equal((2, 3), "asdf\nasdf\nasdf".GetLineAndColumnNumber(7));
+
+            Assert.Equal((3, 4), "asdf\nasdf\nasdf".GetLineAndColumnNumber(13));
+        }
+    }
+}
