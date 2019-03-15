5cc8823aa16dc7f80ca2dbc82ec76bcd81503e70
jonathan.byrne <jonathan.byrne@yahoo.com>
2019-03-15 11:18:36 -0400

    Added extension method to remove query parameter from uri

---
+++ src/ByrneLabs.Commons/UriExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ByrneLabs.Commons
+{
+    public static class UriExtensions
+    {
+        public static Uri RemoveQueryParameter(this Uri uri, string queryParameterName)
+        {
+            if (string.IsNullOrWhiteSpace(uri.Query))
+            {
+                return uri;
+            }
+
+            var uriBuilder = new UriBuilder(uri);
+            var queryParameters = uriBuilder.Query.SubstringAfterLast("?").Split('&').ToList();
+            var queryParametersToRemove = queryParameters.Where(qp => qp.StartsWith(queryParameterName + "=", StringComparison.Ordinal)).ToArray();
+            foreach (var queryParameterToRemove in queryParametersToRemove)
+            {
+                queryParameters.Remove(queryParameterToRemove);
+            }
+
+            uriBuilder.Query = queryParameters.Any() ? "?" + queryParameters.Join("&") : string.Empty;
+
+            return uriBuilder.Uri;
+        }
+    }
+}
---
+++ test/ByrneLabs.Commons.Tests/UriExtensionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace ByrneLabs.Commons.Tests
+{
+    public class UriExtensionTest
+    {
+        [Fact]
+        public void TestRemoveQueryParameter1()
+        {
+            var uri = new Uri("http://some.domain/path1/path2/resource?key1=value1&key2=value2");
+            var newUri = uri.RemoveQueryParameter("key1");
+            Assert.Equal("http://some.domain/path1/path2/resource?key2=value2", newUri.ToString());
+        }
+
+        [Fact]
+        public void TestRemoveQueryParameter2()
+        {
+            var uri = new Uri("http://some.domain/path1/path2/resource?key1=value1&key2=value2&key1=value3");
+            var newUri = uri.RemoveQueryParameter("key1");
+            Assert.Equal("http://some.domain/path1/path2/resource?key2=value2", newUri.ToString());
+        }
+
+        [Fact]
+        public void TestRemoveQueryParameter3()
+        {
+            var uri = new Uri("http://some.domain/path1/path2/resource?key1=value");
+            var newUri = uri.RemoveQueryParameter("key1");
+            Assert.Equal("http://some.domain/path1/path2/resource", newUri.ToString());
+        }
+    }
+}
